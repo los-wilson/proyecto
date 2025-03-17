ab13dbd90322f86ab1ce305965e5b4edbe051c22
Omar <omartrejo@zoho.com>
2025-03-17 00:50:08 -0600

    Imagen de pug

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask('_name_')
 # Define the route for the home page
 @app.route('/')
 def hello_world():
-    return 'Hola mundo!'
+    return '<h1>Wilson es la onda<h1>'
+            '<img src="https://www.muyinteresante.com/wp-content/uploads/sites/5/2022/10/13/6347f72b104ac.jpeg">'
 
 # Run the app
 app.run(debug=True)
